c4778a70ce2895a4aff3f6e7740da84880f03954
Lucie Russeil <lucie.russeil@etu.univ-poitiers.fr>
2024-11-25 17:23:09 +0100

    gestion erreur test tri

--- src/components/CharactersList.test.jsx
+++ src/components/CharactersList.test.jsx
@@ -3,7 +3,7 @@ import '@testing-library/react';
 
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { CharactersList } from './CharactersList';
 
@@ -47,7 +47,28 @@ describe('CharactersList Component', () => {
     expect(screen.getByRole('link', { name: 'Hulk' })).toHaveAttribute('href', '/characters/3');
   });
 
+
 /* PARTIE SUR LA FONCTION QUI ORDONNE LES DONNEES */
+
+  const mockCharacters = [
+    {
+      id: '1009175',
+      name: 'Beast',
+      modified: '2014-01-13T14:48:32-0500',
+    },
+    {
+      id: '1009220',
+      name: 'Captain America',
+      modified: '2020-04-04T19:01:59-0400',
+    },
+    {
+      id: '1009268',
+      name: 'Deadpool',
+      modified: '2020-04-04T19:02:15-0400',
+    },
+  ];
+
+
   it('should sort characters by ID', () => {
     render(
       <BrowserRouter>

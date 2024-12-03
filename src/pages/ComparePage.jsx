707a2ce4050b5860d5a199cc263b3dbdcf44aaf3
Lucie Russeil <lucie.russeil@etu.univ-poitiers.fr>
2024-12-03 17:01:53 +0100

    remplissage des valeurs dans la liste déroulante

--- src/pages/ComparePage.jsx
+++ src/pages/ComparePage.jsx
@@ -1,26 +1,17 @@
 import React from 'react';
+import characters from "../data/characters.json"; // Importez votre fichier JSON
 
 const CompareCharactersPage = () => {
-    // change the title of the page
+    // Change the title of the page
     document.title = "Compare | Marvel App";
 
-    // A supprimer, permet de rendre le composant fonctionnel dans un premier temps
-    const characters = [
-        {
-            name: '...'
-        },{
-            name: '---'
-        }
-    ]
-    // Fin de la partie à supprimer
-
-    // transform the characters to array of label/value objects
-    const options = characters.map((character, index) => ({
-        value: index,
+    // Transform the characters into an array of label/value objects
+    const options = characters.map((character) => ({
+        value: character.id, // Utilisez l'ID comme valeur unique
         label: character.name,
     }));
 
-    // set the default options to the first two characters
+    // Set the default options to the first two characters
     const [option1, setOption1] = React.useState(options[0]);
     const [option2, setOption2] = React.useState(options[1]);
 
@@ -31,13 +22,15 @@ const CompareCharactersPage = () => {
 
     return (
         <>
-            <h2>Compare characters</h2>
+            <h2>Compare Characters</h2>
 
             <p style={centerStyle}>
                 <select
                     data-testid='select-character-1'
                     value={option1.value}
-                    onChange={(event) => setOption1(options[event.target.value])}
+                    onChange={(event) =>
+                        setOption1(options.find(opt => opt.value === event.target.value))
+                    }
                 >
                     {options.map((option) => (
                         <option key={option.value} value={option.value}>
@@ -49,7 +42,9 @@ const CompareCharactersPage = () => {
                 <select
                     data-testid='select-character-2'
                     value={option2.value}
-                    onChange={(event) => setOption2(options[event.target.value])}
+                    onChange={(event) =>
+                        setOption2(options.find(opt => opt.value === event.target.value))
+                    }
                 >
                     {options.map((option) => (
                         <option key={option.value} value={option.value}>
@@ -59,11 +54,10 @@ const CompareCharactersPage = () => {
                 </select>
             </p>
 
-            { /* A supprimer, permet le voir comment récupérer les éléments selectionnés */ }
+            {/* Display the comparison */}
             <p style={centerStyle}>
-                {characters[option1.value].name} vs {characters[option2.value].name}
+                Comparing <strong>{option1.label}</strong> with <strong>{option2.label}</strong>
             </p>
-            { /* Fin de la partie à supprimer */ }
         </>
     );
 };
